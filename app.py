6846706b67dd493eb03825fd9207d7bd36335c9c
Skyler Daché <skyberskyman@gmail.com>
2016-05-10 03:52:00 -0400

    added a few files, removed database bullshit

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
 
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def root():
+    return app.send_static_file('index.html')
 
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
 
 if __name__ == '__main__':
     app.run()
